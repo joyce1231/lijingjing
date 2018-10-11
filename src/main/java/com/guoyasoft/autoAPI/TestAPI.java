7d073d75770b4a6b4edf756c181de60418da0e88
吴令 <wx_da9ca5d160264260a28c6febefd0c909@git.code.tencent.com>
2018-10-11 19:03:12 +0800

    '添加接口测试用例'

--- src/main/java/com/guoyasoft/autoAPI/TestAPI.java
+++ src/main/java/com/guoyasoft/autoAPI/TestAPI.java
@@ -19,11 +19,13 @@ public class TestAPI {
     public void testAddAccount(){
       String url="http://qa.guoyasoft.com:8080/account/add";
       Map map=new HashMap<>();
-      map.put("accountName", "asdf000");
+      map.put("accountName", "asdf0007");
       map.put("customerName", "陈超");
       String result=HttpClientUtil.doPost(url, map);
       System.out.println(result);
-      boolean actual=result.contains("0000");
+      boolean actual=result.contains("\"respCode\":\"0000\"");
       Assert.assertEquals(actual, true);
     }
+
+
 }
